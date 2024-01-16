178bcd5c841b56ee7f6995814cc581e6abc83e73
SungJun Kang <130119769+KangSJ98@users.noreply.github.com>
2024-01-16 16:37:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -26,6 +26,7 @@ import pygame
 import sys
 import pickle
 import os
+import numpy as np
 from datetime import datetime
 from InitData.Block_Shape_345DB import b_database
 from InitData.b_sequence import b1_sequence, b2_sequence
@@ -131,10 +132,11 @@ def move_shape(current_shape, shape_position, current_map, map_width, map_height
     return shape_position, can_move
 
 def hard_drop(current_shape, shape_position, current_map, map_width, map_height):
+    position_copy = shape_position
     can_move = True
     while can_move:
-        shape_position, can_move = move_shape(current_shape, shape_position, current_map, map_width, map_height, 0, 1)
-    return shape_position
+        position_copy, can_move = move_shape(current_shape, position_copy, current_map, map_width, map_height, 0, 1)
+    return position_copy
 
 def rotate_clockwise(array):
     return [list(row) for row in zip(*reversed(array))]
@@ -189,29 +191,35 @@ def save_log(map_control_b1, map_control_b2, next_map_control_b1, next_map_contr
 def option_score_left(target):
     target_copy = [row[:] for row in target]
     orphan_result_databases = {}
+    flatness_result_databases = {}
     for shape_name, matrix in b_database.items():
         shape_position = [[0,2], [0,0], [0,0]] # a(0,0~4), a(0~4,0), b(0~width, 0)
         min_hole = float('inf')
+        min_flat = float('inf')
 
         for i in range(5):
             rotated_matrix = rotate_counterclockwise(matrix)
-            shape_position[1][0] = i
+            shape_position[1] = [i,0]
+            shape_position[2] = [0,0]
             if rotated_matrix[0][shape_position[1][0]] == 1 or check_b_possible(rotated_matrix, hard_drop(shape_control_b, shape_position[1], rotated_matrix, WIDTH_B, HEIGHT_B)):
                 continue
             else:
                 rotated_matrix[shape_position[1][1]][shape_position[1][0]] = 1
-            
+                
             combine_matrix = rotate_clockwise(rotated_matrix)
             left = hard_drop(rotate_counterclockwise(combine_matrix), shape_position[2], rotate_counterclockwise(target_copy), HEIGHT_TARGET, WIDTH_TARGET)
-            right = hard_drop(rotate_clockwise(combine_matrix), shape_position[2], rotate_clockwise(target_copy), HEIGHT_TARGET, WIDTH_TARGET)
-            
-            for j in range(WIDTH_TARGET - left[1], right[1]):
+            right = hard_drop(rotate_clockwise(combine_matrix), [HEIGHT_TARGET-HEIGHT_B,0], rotate_clockwise(target_copy), HEIGHT_TARGET, WIDTH_TARGET)
+            for j in range(WIDTH_TARGET - left[1] - WIDTH_B, right[1]):
                 shape_position[2] = hard_drop(combine_matrix, [j,0], target_copy, WIDTH_TARGET, HEIGHT_TARGET)
                 hole = count_orphan_hole(target_copy, combine_matrix, shape_position[2])
+                flatness = measure_flatness_std(target_copy, combine_matrix, shape_position[2])
                 if hole < min_hole:
                     min_hole = hole
+                if flatness < min_flat:
+                    min_flat = flatness
         orphan_result_databases[shape_name] = min_hole
-    return orphan_result_databases
+        flatness_result_databases[shape_name] = min_flat
+    return orphan_result_databases, flatness_result_databases
 
 def count_orphan_hole(target, combine_matrix, shape_position):
     target_copy = [row[:] for row in target]
@@ -224,17 +232,30 @@ def count_orphan_hole(target, combine_matrix, shape_position):
     for y, row in enumerate(target_copy):
         for x, value in enumerate(row):
             if value == 0:
-                for i in range(y):
+                for i in range(y,0,-1):
                     if target_copy[i][x] == 2:
                         hole += 1
+                        break
+                    elif target_copy[i][x] == 1:
                         break
 
     return hole
 
-def count_flat(target, combine_matrix, shape_position)
-
-
+def measure_flatness_std(target, combine_matrix, shape_position):
+    target_copy = [row[:] for row in target]
+    target_height = [sum(column) for column in zip(*target_copy)]
+    std_target = np.std(target_height)
     
+    for y, row in enumerate(combine_matrix):
+        for x, value in enumerate(row):
+            if value:
+                target_copy[shape_position[1] + y][shape_position[0] + x] = 2
+    
+    combine_height = [sum(column) for column in zip(*target_copy)]
+    std_combine = np.std(combine_height)
+
+    return std_combine - std_target
+
 
 clock = pygame.time.Clock()
 game_over = False
@@ -300,7 +321,8 @@ while not game_over:
 
                     # check option b is possible
                     # if impossible : go to phase 1
-                    if rotated_map[0][shape_position[1][0]] == 1 or check_b_possible(rotated_map, hard_drop(shape_control_b, shape_position[1], rotated_map, WIDTH_B, HEIGHT_B)):
+                    shape_position[1] = hard_drop(shape_control_b, shape_position[1], rotated_map, WIDTH_B, HEIGHT_B)
+                    if rotated_map[0][shape_position[1][0]] == 1 or check_b_possible(rotated_map, shape_position[1]):
                         rotated_map[shape_position[1][1]][shape_position[1][0]] = 3
                         stage_end = True
                         error_b = True
@@ -319,7 +341,8 @@ while not game_over:
 
                     # check option b is possible
                     # if impossible : go to phase 1
-                    if rotated_map[0][shape_position[1][0]] == 1 or check_b_possible(rotated_map, hard_drop(shape_control_b, shape_position[1], rotated_map, WIDTH_B, HEIGHT_B)):
+                    shape_position[1] = hard_drop(shape_control_b, shape_position[1], rotated_map, WIDTH_B, HEIGHT_B)
+                    if rotated_map[0][shape_position[1][0]] == 1 or check_b_possible(rotated_map, shape_position[1]):
                         rotated_map[shape_position[1][1]][shape_position[1][0]] = 3
                         stage_end = True
                         error_b = True
@@ -353,8 +376,10 @@ while not game_over:
                 map_target, score = remove_line(map_target,score)
                 trial_over = check_trial_over(map_target)
                 if move == 5:
-                    orphan_data = option_score_left(map_target)
+                    orphan_data, flat_data = option_score_left(map_target)
                     for shape_name, value in orphan_data.items():
+                        print(f"{shape_name} 결과 : {value}")
+                    for shape_name, value in flat_data.items():
                         print(f"{shape_name} 결과 : {value}")
 
             ### visualization
