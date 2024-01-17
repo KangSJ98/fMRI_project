a80a8f8921ec7d4224fb6f8e2db8363692369287
SungJun Kang <130119769+KangSJ98@users.noreply.github.com>
2024-01-17 14:53:36 +0900

    Update option_score.py

--- option_score.py
+++ option_score.py
@@ -18,11 +18,15 @@ def option_score_left(target):
     target_copy = [row[:] for row in target]
     orphan_result_databases = {}
     flatness_result_databases = {}
+    border_result_databases = {}
+
     for shape_name, matrix in b_database.items():
         shape_position = [[0,2], [0,0], [0,0]] # a(0,0~4), a(0~4,0), b(0~width, 0)
         min_hole = float('inf')
         min_flat = float('inf')
-
+        min_border = float('inf')
+        mini, minj = 0, 0
+        # for all option a
         for i in range(5):
             rotated_matrix = rotate_counterclockwise(matrix)
             shape_position[1] = [i,0]
@@ -35,17 +39,26 @@ def option_score_left(target):
             combine_matrix = rotate_clockwise(rotated_matrix)
             left = hard_drop(rotate_counterclockwise(combine_matrix), shape_position[2], rotate_counterclockwise(target_copy), HEIGHT_TARGET, WIDTH_TARGET)
             right = hard_drop(rotate_clockwise(combine_matrix), [HEIGHT_TARGET-HEIGHT_B,0], rotate_clockwise(target_copy), HEIGHT_TARGET, WIDTH_TARGET)
+
+            # for all possible location of combine shape 
             for j in range(WIDTH_TARGET - left[1] - WIDTH_B, right[1]):
                 shape_position[2] = hard_drop(combine_matrix, [j,0], target_copy, WIDTH_TARGET, HEIGHT_TARGET)
                 hole = count_orphan_hole(target_copy, combine_matrix, shape_position[2])
                 flatness = measure_flatness_std(target_copy, combine_matrix, shape_position[2])
-                if hole < min_hole:
-                    min_hole = hole
-                if flatness < min_flat:
-                    min_flat = flatness
+                border = count_border(target_copy, combine_matrix, shape_position[2])
+
+                min_hole = min(min_hole, hole)
+                min_flat = min(min_flat, flatness)
+                if border < min_border:
+                    mini = i
+                    minj = j
+                min_border = min(min_border, border)
+
         orphan_result_databases[shape_name] = min_hole
         flatness_result_databases[shape_name] = min_flat
-    return orphan_result_databases, flatness_result_databases
+        border_result_databases[shape_name] = min_border
+        print(f"i : {mini}, j : {minj}")
+    return orphan_result_databases, flatness_result_databases, border_result_databases
 
 def count_orphan_hole(target, combine_matrix, shape_position):
     target_copy = [row[:] for row in target]
@@ -81,3 +94,38 @@ def measure_flatness_std(target, combine_matrix, shape_position):
     std_combine = np.std(combine_height)
 
     return std_combine - std_target
+
+def count_border(target, combine_matrix, shape_position):
+    target_copy = [row[:] for row in target]
+    total_count_1 = 0
+    total_count_2 = 0
+    
+    for y, row in enumerate(combine_matrix):
+        for x, value in enumerate(row):
+            if value:
+                target_copy[shape_position[1] + y][shape_position[0] + x] = 2
+    
+    for y, row in enumerate(target_copy):
+        for x, value in enumerate(row):
+            if value == 2:
+                count_ones, count_twos = count_neighbors(target_copy, y, x)
+                total_count_1 += count_ones
+                total_count_2 += count_twos
+    border_combine = np.count_nonzero(combine_matrix) * 4 - total_count_2
+    border_target = border_combine - total_count_1
+    return border_target / border_combine
+
+def count_neighbors(target, row, col):
+    count_1 = 0
+    count_2 = 0
+
+    neighbors = [(row-1, col), (row+1,col), (row, col-1), (row, col+1)]
+
+    for r, c in neighbors:
+        if 0 <= r < len(target) and 0 <= c < len(target[0]):
+            if target[r][c] == 1:
+                count_1 += 1
+            elif target[r][c] == 2:
+                count_2 += 1
+    
+    return count_1, count_2
