3faf2e2d9d070698d35459748ce20ce0cdfe280c
SungJun Kang <130119769+KangSJ98@users.noreply.github.com>
2024-02-29 00:56:19 +0900

    Update GetUserSelection.m

--- set_task/utils/GetUserSelection.m
+++ set_task/utils/GetUserSelection.m
@@ -2,9 +2,10 @@ function selection = GetUserSelection()
 selection = randi([1, 2]); % 랜덤 선택
 
 startTime = GetSecs; % 현재 시간 기록
-while GetSecs - startTime <= 10
+while GetSecs - startTime <= 1
+end
+while GetSecs - startTime <= 4
     [keyIsDown, ~, keyCode] = KbCheck;
-    disp(keyIsDown);
     if keyIsDown == true
         if keyCode(KbName('LeftArrow'))
             selection = 1;
@@ -12,6 +13,8 @@ while GetSecs - startTime <= 10
         elseif keyCode(KbName('RightArrow'))
             selection = 2;
             break;
+        elseif keyCode(KbName('Escape'))
+            selection = 3;
         end
     end
 end
