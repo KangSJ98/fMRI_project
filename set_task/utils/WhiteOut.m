2fdf43c8252e81bb92d1f427e3e38ccb559931fa
SungJun Kang <130119769+KangSJ98@users.noreply.github.com>
2024-03-07 11:17:36 +0900

    fix bug

--- set_task/utils/WhiteOut.m
+++ set_task/utils/WhiteOut.m
@@ -1,5 +1,5 @@
 function WhiteOut(opt)
-Screen('FillRect', opt.window, [0 0 0], [0, 0, opt.screenWeight, opt.screenHeight]);
+Screen('FillRect', opt.window, [255 255 255], [0, 0, opt.screenWidth, opt.screenHeight]);
 Screen('Flip', opt.window);
 startTime = GetSecs;
 while GetSecs - startTime <= 0.5 + (0.7 - 0.5) * rand
